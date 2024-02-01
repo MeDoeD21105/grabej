6b6e753b97b98e45eb3ce2c403fa02020732b9e3
MeDoeD です <medoed21105@gmail.com>
2024-02-02 00:47:49 +0300

    added product in model and test website, it's wery goood. IT WORK

--- Medoed/MedoedPJ/views.py
+++ Medoed/MedoedPJ/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import *
+
 
 # Create your views here.
 
 def index(request):
-    return render(request, "MedoedPJ/index.html")
+    post = Product.objects.all()
+    return render(request, "MedoedPJ/index.html",{"posts" : post})
