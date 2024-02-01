913ab89afa647a34a92c8c37828cf498d55ebefd
MeDoeD です <medoed21105@gmail.com>
2024-02-02 01:05:09 +0300

    added админ панель , название категорий и мелкие расширения, IT WOOORK

--- Medoed/MedoedPJ/admin.py
+++ Medoed/MedoedPJ/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 from .models import *
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+     list_display = ("title", "content", "price", "quantity")
 
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
--- Medoed/MedoedPJ/models.py
+++ Medoed/MedoedPJ/models.py
@@ -3,9 +3,9 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class Product(models.Model):
-    title = models.CharField(max_length = 200)
-    content = models.TextField()
-    photo = models.ImageField(upload_to = "photos/ ",blank = True)
-    price = models.DecimalField( max_digits = 7, decimal_places = 2)
-    quantity = models.IntegerField( default=0, validators=[MinValueValidator(0), MaxValueValidator(1000)])
+    title = models.CharField(max_length = 200, verbose_name = "Название")
+    content = models.TextField(verbose_name = "Описание")
+    photo = models.ImageField(upload_to = "photos/ ",blank = True, verbose_name = "Фото")
+    price = models.DecimalField( max_digits = 7, decimal_places = 2, verbose_name = "Цена")
+    quantity = models.IntegerField( default=0, validators=[MinValueValidator(0), MaxValueValidator(1000)], verbose_name = "Количество")
     
