dec71b5491797b54e8137c8af5ad0d4a80b65114
MeDoeD です <medoed21105@gmail.com>
2024-02-02 00:08:20 +0300

    added models Product : name, content, photo, price, quantity

--- Medoed/MedoedPJ/models.py
+++ Medoed/MedoedPJ/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length = 200)
+    content = models.TextField()
+    photo = models.ImageField(upload_to = "photos/ ",blank = True)
+    price = models.DecimalField( max_digits = 7, decimal_places = 2)
+    quantity = models.IntegerField( default=0, validators=[MinValueValidator(0), MaxValueValidator(1000)])
+    
