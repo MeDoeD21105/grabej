2033975ba122d1dcb874fab2a0885e549973e5f1
MeDoeD です <medoed21105@gmail.com>
2024-02-02 02:13:39 +0300

    added class view, modif index.html and modif urls

--- Medoed/MedoedPJ/views.py
+++ Medoed/MedoedPJ/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from .models import *
 
+from django.views.generic import ListView
 
 # Create your views here.
 
-def index(request):
-    post = Product.objects.all()
-    return render(request, "MedoedPJ/index.html",{"posts" : post})
+
+
+class ProductClass(ListView):
+    model = Product
+    template_name = "MedoedPJ/index.html"
+    context_object_name = 'posts'
+    
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["titlle"] = "grabej"
+        return context
+    
