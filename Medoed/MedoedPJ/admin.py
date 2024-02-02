796ba05c77c6f026d273e73eea4e7bbc535f7ded
MeDoeD です <medoed21105@gmail.com>
2024-02-02 17:25:58 +0300

    added sellserr in admin.py and meelee modif

--- Medoed/MedoedPJ/admin.py
+++ Medoed/MedoedPJ/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 class ProductAdmin(admin.ModelAdmin):
-     list_display = ("title", "content", "price", "quantity")
+     list_display = ("title", "content", "price", "quantity", "sellerr")
 
 admin.site.register(Product, ProductAdmin)
--- Medoed/MedoedPJ/views.py
+++ Medoed/MedoedPJ/views.py
@@ -14,6 +14,7 @@ class ProductClass(ListView):
     
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["titlle"] = "grabej"
+        context["title"] = "grabej "
+        
         return context
     
