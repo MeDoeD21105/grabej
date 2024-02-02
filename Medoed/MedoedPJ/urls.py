6d85c7fc6ee486531f7b1b7a480f452b19848642
MeDoeD です <medoed21105@gmail.com>
2024-02-03 01:06:36 +0300

    small modif

--- Medoed/MedoedPJ/urls.py
+++ Medoed/MedoedPJ/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("index/", ProductClass.as_view()),
+    path("index/", ProductClass.as_view(), name="home2"),
     path("", base),
 ]
