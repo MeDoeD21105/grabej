6a355bee0be1b5fa9990b4aaf8ad437df7476866
MeDoeD です <medoed21105@gmail.com>
2024-02-26 22:35:51 +0300

    male modif

--- Medoed/MedoedPJ/forms.py
+++ Medoed/MedoedPJ/forms.py
@@ -1,6 +1,7 @@
 from django.forms import ModelForm
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import get_user_model
 from .models import *
 
 class Added_ProdForm(ModelForm):
@@ -15,7 +16,11 @@ class LoginUserForm(AuthenticationForm):
     
     
     
-class RegisterUserForm(UserCreationForm):
+class RegisterUserForm(forms.ModelForm):
     username = forms.CharField(label="Логин")
-    password = forms.CharField(label="Пороль")
+    password = forms.CharField(label="Пароль",  widget=forms.PasswordInput())
+    password2 = forms.CharField(label=" Повтор пароля", widget=forms.PasswordInput())
     
+    class Meta:
+        model = get_user_model()
+        fields = ["username", "password", "password2" ]
